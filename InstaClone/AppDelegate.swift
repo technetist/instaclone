33bb8866440a63f293db93e23b6d954080f7b880
Adrien Maranville <adrien.maranville@gmail.com>
2017-04-07 22:55:16 -0600

    added user table and follower functions

--- InstaClone/AppDelegate.swift
+++ InstaClone/AppDelegate.swift
@@ -1,46 +1,158 @@
-//
-//  AppDelegate.swift
-//  InstaClone
-//
-//  Created by Adrien Maranville on 4/1/17.
-//  Copyright © 2017 Adrien Maranville. All rights reserved.
-//
+/**
+* Copyright (c) 2015-present, Parse, LLC.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree. An additional grant
+* of patent rights can be found in the PATENTS file in the same directory.
+*/
 
 import UIKit
+
+import Parse
+
+// If you want to use any of the UI components, uncomment this line
+// import ParseUI
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    //--------------------------------------
+    // MARK: - UIApplicationDelegate
+    //--------------------------------------
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Enable storing and querying data from Local Datastore.
+        // Remove this line if you don't want to use Local Datastore features or want to use cachePolicy.
+        Parse.enableLocalDatastore()
+        
+        let parseConfiguration = ParseClientConfiguration(block: { (ParseMutableClientConfiguration) -> Void in
+            ParseMutableClientConfiguration.applicationId = "f794e24dbbdb6a9e0aec867539ca59d941e33e92"
+            ParseMutableClientConfiguration.clientKey = "7af2f1bc24270616190e218c38658544f551fcbf"
+            ParseMutableClientConfiguration.server = "http://ec2-54-201-16-29.us-west-2.compute.amazonaws.com:80/parse"
+        })
+        
+        Parse.initialize(with: parseConfiguration)
+
+
+        // ****************************************************************************
+        // Uncomment and fill in with your Parse credentials:
+        // Parse.setApplicationId("your_application_id", clientKey: "your_client_key")
+        //
+        // If you are using Facebook, uncomment and add your FacebookAppID to your bundle's plist as
+        // described here: https://developers.facebook.com/docs/getting-started/facebook-sdk-for-ios/
+        // Uncomment the line inside ParseStartProject-Bridging-Header and the following line here:
+        // PFFacebookUtils.initializeFacebook()
+        // ****************************************************************************
+
+        //PFUser.enableAutomaticUser()
+
+        let defaultACL = PFACL();
+
+        // If you would like all objects to be private by default, remove this line.
+        defaultACL.getPublicReadAccess = true
+
+        PFACL.setDefault(defaultACL, withAccessForCurrentUser: true)
+
+        if application.applicationState != UIApplicationState.background {
+            // Track an app open here if we launch with a push, unless
+            // "content_available" was used to trigger a background push (introduced in iOS 7).
+            // In that case, we skip tracking here to avoid double counting the app-open.
+            /*
+            let preBackgroundPush = !application.responds(to: #selector(getter: UIApplication.backgroundRefreshStatus))
+            let oldPushHandlerOnly = !self.responds(to: #selector(UIApplicationDelegate.application(_:didReceiveRemoteNotification:fetchCompletionHandler:)))
+            var noPushPayload = false;
+            if let options = launchOptions {
+                noPushPayload = options[UIApplicationLaunchOptionsRemoteNotificationKey] != nil;
+            }
+            if (preBackgroundPush || oldPushHandlerOnly || noPushPayload) {
+                PFAnalytics.trackAppOpened(launchOptions: launchOptions)
+            }
+ */
+        }
+
+        //
+        //  Swift 1.2
+        //
+        //        if application.respondsToSelector("registerUserNotificationSettings:") {
+        //            let userNotificationTypes = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound
+        //            let settings = UIUserNotificationSettings(forTypes: userNotificationTypes, categories: nil)
+        //            application.registerUserNotificationSettings(settings)
+        //            application.registerForRemoteNotifications()
+        //        } else {
+        //            let types = UIRemoteNotificationType.Badge | UIRemoteNotificationType.Alert | UIRemoteNotificationType.Sound
+        //            application.registerForRemoteNotificationTypes(types)
+        //        }
+
+        //
+        //  Swift 2.0
+        //
+        //        if #available(iOS 8.0, *) {
+        //            let types: UIUserNotificationType = [.Alert, .Badge, .Sound]
+        //            let settings = UIUserNotificationSettings(forTypes: types, categories: nil)
+        //            application.registerUserNotificationSettings(settings)
+        //            application.registerForRemoteNotifications()
+        //        } else {
+        //            let types: UIRemoteNotificationType = [.Alert, .Badge, .Sound]
+        //            application.registerForRemoteNotificationTypes(types)
+        //        }
+
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    //--------------------------------------
+    // MARK: Push Notifications
+    //--------------------------------------
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let installation = PFInstallation.current()
+        installation.setDeviceTokenFrom(deviceToken)
+        installation.saveInBackground()
+
+        PFPush.subscribeToChannel(inBackground: "") { (succeeded, error) in // (succeeded: Bool, error: NSError?) is now (succeeded, error)
+
+            if succeeded {
+                print("ParseStarterProject successfully subscribed to push notifications on the broadcast channel.\n");
+            } else {
+                print("ParseStarterProject failed to subscribe to push notifications on the broadcast channel with error = %@.\n", error)
+            }
+        }
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        if error.code == 3010 {
+            print("Push notifications are not supported in the iOS Simulator.\n")
+        } else {
+            print("application:didFailToRegisterForRemoteNotificationsWithError: %@\n", error)
+        }
     }
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        PFPush.handle(userInfo)
+        if application.applicationState == UIApplicationState.inactive {
+            PFAnalytics.trackAppOpened(withRemoteNotificationPayload: userInfo)
+        }
     }
 
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
+    ///////////////////////////////////////////////////////////
+    // Uncomment this method if you want to use Push Notifications with Background App Refresh
+    ///////////////////////////////////////////////////////////
+    // func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject], fetchCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
+    //     if application.applicationState == UIApplicationState.Inactive {
+    //         PFAnalytics.trackAppOpenedWithRemoteNotificationPayload(userInfo)
+    //     }
+    // }
 
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
+    //--------------------------------------
+    // MARK: Facebook SDK Integration
+    //--------------------------------------
 
-
+    ///////////////////////////////////////////////////////////
+    // Uncomment this method if you are using Facebook
+    ///////////////////////////////////////////////////////////
+    // func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
+    //     return FBAppCall.handleOpenURL(url, sourceApplication:sourceApplication, session:PFFacebookUtils.session())
+    // }
 }
-
--- InstaClone/ViewController.swift
+++ InstaClone/ViewController.swift
@@ -7,9 +7,125 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
+    
+    var signupMode = true
+    
+    var activityIndicator = UIActivityIndicatorView()
+    
+    func createAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+            self.dismiss(animated: true, completion: nil)
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
 
+    @IBOutlet weak var txtBoxEmail: UITextField!
+    @IBOutlet weak var txtBoxPassword: UITextField!
+    @IBOutlet weak var btnSignupOrLogin: UIButton!
+    @IBAction func btnSignupOrLoginPressed(_ sender: Any) {
+        if txtBoxEmail.text == "" || txtBoxPassword.text == "" {
+            createAlert(title: "Ooops!", message: "Please enter an email or password")
+            
+            
+        } else {
+            
+            activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            activityIndicator.center = self.view.center
+            activityIndicator.hidesWhenStopped = true
+            activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+            view.addSubview(activityIndicator)
+            activityIndicator.startAnimating()
+            UIApplication.shared.beginIgnoringInteractionEvents()
+            
+            if signupMode {
+                //sign user up
+                
+                let user = PFUser()
+                
+                user.username = txtBoxEmail.text
+                user.email = txtBoxEmail.text
+                user.password = txtBoxPassword.text
+                
+                user.signUpInBackground(block: { (success, error) in
+                    
+                    self.activityIndicator.stopAnimating()
+                    UIApplication.shared.endIgnoringInteractionEvents()
+                    
+                    if error != nil {
+                        var displayErrorMessage = "Please try again in a bit"
+                        if let errorMessage = error as NSError? {
+                            displayErrorMessage = errorMessage.userInfo["error"] as! String
+                        }
+                        self.createAlert(title: "Signup Error", message: displayErrorMessage)
+                    } else {
+                        print("User registered")
+                        
+                        self.performSegue(withIdentifier: "showUserTable", sender: self)
+                    }
+                    
+                })
+            } else {
+                //user log in
+                
+                PFUser.logInWithUsername(inBackground: txtBoxEmail.text!, password: txtBoxPassword.text!, block: { (user, error) in
+                    
+                    self.activityIndicator.stopAnimating()
+                    UIApplication.shared.endIgnoringInteractionEvents()
+                    
+                    if error != nil {
+                        var displayErrorMessage = "Please try again in a bit"
+                        if let errorMessage = error as NSError? {
+                            displayErrorMessage = errorMessage.userInfo["error"] as! String
+                        }
+                        self.createAlert(title: "Login Error", message: displayErrorMessage)
+                    } else {
+                        print("Logged in")
+                        self.performSegue(withIdentifier: "showUserTable", sender: self)
+                    }
+                })
+                
+            }
+        }
+        
+    }
+    @IBOutlet weak var btnChangeSigninMode: UIButton!
+    @IBAction func btnChangeSigninModePressed(_ sender: Any) {
+        if signupMode {
+            //change to log in mode
+            
+            btnSignupOrLogin.setTitle("Log In", for: [])
+            
+            btnChangeSigninMode.setTitle("Sign Up", for: [])
+            
+            lblHaveAccountMessage.text = "Don't have an account?"
+            
+            signupMode = false
+            
+        } else {
+            //change to sign up mode
+            
+            btnSignupOrLogin.setTitle("Sign Up", for: [])
+            
+            btnChangeSigninMode.setTitle("Log In", for: [])
+            
+            lblHaveAccountMessage.text = "Already have an account?"
+            
+            signupMode = true
+        }
+    }
+    @IBOutlet weak var lblHaveAccountMessage: UILabel!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if PFUser.current() != nil {
+            performSegue(withIdentifier: "showUserTable", sender: self)
+        }
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
